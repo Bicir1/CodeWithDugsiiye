17a78aba91afaad760964d4d62a84343ea65b759
Abdifitaah Mohamed Yusuf <yuusufmoharaage@gmail.com>
2025-10-30 20:31:04 +0300

    commited

--- REACT/getten-started/src/App.jsx
+++ REACT/getten-started/src/App.jsx
@@ -1,15 +1,14 @@
-import React, { useState } from 'react';
+import { useState } from "react";
 
 const App = () => {
-    const [count, setCount] = useState(0);
-    const increment =()=>{
-        setCount(count +1)
-        console.log(count)
-    } 
+    const [isVisible , setisVisible] = useState(false);
+    const toggle =() => {
+        setisVisible(!isVisible)
+    }
     return (
         <div>
-            <p>You clicked {count} times</p>
-            <button onClick={increment}>Click Me</button>
+            <button onClick={toggle}>{isVisible ? "Hide" : "Show"}Message</button>
+            {isVisible && <p>This is message</p>}
         </div>
     );
 };
