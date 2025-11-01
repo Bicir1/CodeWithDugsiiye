054bc6286752b0d8eba2b5f37231feac642c2abe
Abdifitaah Mohamed Yusuf <yuusufmoharaage@gmail.com>
2025-11-01 11:18:39 +0300

    commited

---
+++ REACT/exercise-4/src/Toggalle.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Toggalle = () => {
+    return (
+        <div>
+            <p>the button is {}</p>
+            <button>Trun </button>
+        </div>
+    );
+};
+
+export default Toggalle;
